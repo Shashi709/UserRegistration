ba258f530c0f1d3d6ec82c5f8df8b7bb2d58035a
Shashi709 <shashimsi63@gmail.com>
2023-01-13 00:21:59 +0530

    adding backend link in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import RegisterPage from './component/RegisterPage';
-export const SERVER_URL = "http://localhost:4000"
-
+//export const SERVER_URL = "http://localhost:4000"
+export const SERVER_URL = "https://userregistrationbackend.onrender.com"
 function App() {
   return (
     <div className="App">
